246c015993a76b690bb49c58eff18ff883ad97c2
Preston West <prestonmontewest@gmail.com>
2020-01-11 13:48:12 -0600

    Changed website title, added calendar icon to date published, and changed border animations for post list items

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,6 +7,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'Preston Monte West';
+  title = 'preston-monte-west';
   year = (new Date()).getFullYear();
 }
