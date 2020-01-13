23b764af989c71a9066bd68da76e5bb351f617df
Preston West <preston.west@cyxtera.com>
2020-01-13 13:15:50 -0600

    Updated website title and updated post list

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,11 @@
-import { Component } from '@angular/core';
-
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.scss"]
 })
 export class AppComponent {
-  title = 'preston-monte-west';
-  year = (new Date()).getFullYear();
+  title = "Preston Monte West";
+  year = new Date().getFullYear();
 }
