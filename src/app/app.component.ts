75e247a649f87a7a23993f1616ebd22f19a9f7d3
Preston West <preston.west@cyxtera.com>
2020-01-21 16:16:07 -0600

    Added auth service and login/logout button

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Location } from '@angular/common';
+
+import { AuthService } from './auth.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Preston Monte West';
   year = new Date().getFullYear();
+  route: string;
+
+  constructor(public auth: AuthService, private router: Router, private location: Location) {}
+
+  ngOnInit(): void {
+    this.router.events.subscribe((value) => {
+      if (this.location.path() !== '') {
+        this.route = this.location.path();
+      } else {
+        this.route = '/';
+      }
+    });
+  }
 }
