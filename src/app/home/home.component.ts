5d42714cca3f8eba1efdbc4918a6c977e62e394c
Preston West <preston.west@cyxtera.com>
2020-03-02 13:56:05 -0600

    Added redirecting to badges

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -18,4 +18,16 @@ export class HomeComponent implements OnInit {
       content: "Preston's personal site for all things related to him."
     })
   }
+
+  clickPractitionerBadge(): void {
+    window.location.href = 'https://www.certmetrics.com/amazon/public/badge.aspx?i=9&t=c&d=2018-10-15&ci=AWS00602410';
+  }
+
+  clickDeveloperBadge(): void {
+    window.location.href = 'https://www.certmetrics.com/amazon/public/badge.aspx?i=2&t=c&d=2018-09-04&ci=AWS00602410';
+  }
+
+  clickArchitectBadge(): void {
+    window.location.href = 'https://www.certmetrics.com/amazon/public/badge.aspx?i=1&t=c&d=2019-01-25&ci=AWS00602410';
+  }
 }
