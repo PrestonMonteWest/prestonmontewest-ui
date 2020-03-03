35121d25029c70eeaa395eb0d23ad89fc6caff62
Preston West <preston.west@cyxtera.com>
2020-03-03 13:52:07 -0600

    Removed home component in favor of making post the homepage

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-import { HomeComponent } from './home/home.component';
-
 const routes: Routes = [
-  { path: '', redirectTo: 'home', pathMatch: 'full' },
-  { path: 'home', component: HomeComponent },
+  { path: '', redirectTo: 'post', pathMatch: 'full' },
   { path: 'post', loadChildren: () => import('./post/post.module').then(m => m.PostModule) },
   { path: 'project', loadChildren: () => import('./project/project.module').then(m => m.ProjectModule) },
-  { path: '**', redirectTo: 'home' }
+  { path: '**', redirectTo: 'post' }
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule, Title, Meta } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
 
 import { AppRoutingModule } from './app-routing.module';
 import { SharedModule } from './shared/shared.module';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    HomeComponent
-  ],
+  declarations: [ AppComponent ],
   imports: [
     HttpClientModule,
     BrowserModule,
