3c83a6ffd6c7ceb52a096165a32827e2eab5407d
Preston West <prestonmontewest@gmail.com>
2020-04-28 21:07:31 -0500

    Moved app-social component to left side of page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,10 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'post', pathMatch: 'full' },
-  { path: 'post', loadChildren: () => import('./post/post.module').then(m => m.PostModule) },
+  { path: '', redirectTo: 'blog', pathMatch: 'full' },
+  { path: 'blog', loadChildren: () => import('./post/post.module').then(m => m.PostModule) },
   { path: 'project', loadChildren: () => import('./project/project.module').then(m => m.ProjectModule) },
-  { path: '**', redirectTo: 'post' }
+  { path: '**', redirectTo: 'blog' }
 ];
 
 @NgModule({
