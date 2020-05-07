ed13686c5f910f985460208fe9fee4f3a05b76d9
Preston West <prestonmontewest@gmail.com>
2020-05-06 21:37:38 -0500

    Updated import to be absolute

--- src/app/interceptor.service.spec.ts
+++ src/app/interceptor.service.spec.ts
@@ -1,6 +1,6 @@
 import { TestBed } from '@angular/core/testing';
 
-import { InterceptorService } from './interceptor.service';
+import { InterceptorService } from 'src/app/interceptor.service';
 
 describe('InterceptorService', () => {
   let service: InterceptorService;
