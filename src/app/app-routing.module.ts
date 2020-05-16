ecdda29a1c2b388509baf1c6695d2bede446b23b
Preston West <prestonmontewest@gmail.com>
2020-05-15 22:59:17 -0500

    Removed project from app routing module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,7 +8,6 @@ import { NotFoundComponent } from './not-found/not-found.component';
 const routes: Routes = [
   { path: '', redirectTo: 'blog', pathMatch: 'full' },
   { path: 'blog', loadChildren: () => import('./post/post.module').then(m => m.PostModule) },
-  { path: 'project', loadChildren: () => import('./project/project.module').then(m => m.ProjectModule) },
   { path: 'not-found', pathMatch: 'full', component: NotFoundComponent },
   { path: '**', redirectTo: 'not-found' }
 ];
